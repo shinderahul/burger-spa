e4345b4492e262a67c0f24ea4d22e44a20932146
Rahul Shinde <rahul.shinde@digitaslbi.com>
2018-03-08 16:03:48 +0530

    Outputing multiple build controls

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,7 +4,7 @@ import classes from './BuildControl.css';
 
 const BuildControl = props => (
   <div className={classes.BuildControl}>
-    <div className={classes.label}>{props.label}</div>
+    <div className={classes.Label}>{props.label}</div>
     <button className={classes.Less}>Less</button>
     <button className={classes.More}>More</button>
   </div>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,26 +2,26 @@ import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
-
-    state = {
-        ingredients: {
-            salad: 0,
-            bacon: 0,
-            cheese: 0,
-            meat: 0
-        }
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
+  };
 
-    render() {
-        return (
-            <Aux>
-                <Burger ingredients={this.state.ingredients} />
-                <div>Build Control</div>
-            </Aux>
-        );
-    }
+  render() {
+    return (
+      <Aux>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls />
+      </Aux>
+    );
+  }
 }
 
 export default BurgerBuilder;
