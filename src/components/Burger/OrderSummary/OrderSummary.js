278ddb7db25a2276ee72b6dd2a8404ed9f13caa6
Rahul Shinde <rahul.shinde@digitaslbi.com>
2018-03-13 23:32:21 +0530

    Fixed decimal number in Model

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -19,7 +19,7 @@ const OrderSummary = props => {
       <p>A delicious burger with the following ingredients</p>
       <ul>{ingredientsSummary}</ul>
       <p>
-        <strong>Total Price: {props.price}</strong>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
       </p>
       <p>Continue to Checkout?</p>
       <Button btnType="Danger" clicked={props.purchaseCancelled}>
