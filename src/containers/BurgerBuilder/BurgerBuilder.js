01cfc808288784fd7d58c718f61c1c78b9471237
Rahul Shinde <rahul.shinde@digitaslbi.com>
2018-03-20 00:01:24 +0530

    Sent post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -81,7 +82,25 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert('You Continue');
+    //alert('You Continue');
+    const orders = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Rahul Shinde',
+        address: {
+          street: 'Test street 1',
+          zipcode: '400123',
+          country: 'India'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'fastest'
+    };
+    axios
+      .post('/orders.json', orders)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   };
 
   render() {
